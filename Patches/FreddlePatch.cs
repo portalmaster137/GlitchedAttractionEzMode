8a4dacaf7be560cb33fb8e569eb1ca0a18db31bc
Portalmaster137 <portalmaster137@gmail.com>
2022-11-15 04:53:32 +1000

    Freddle Patch

---
+++ Patches/FreddlePatch.cs
@@ -0,0 +1,22 @@
+using GlitchedAttraction;
+using System.Reflection;
+using HarmonyLib;
+
+public class FreddlePatch : IPatch
+{
+    public MethodBase Original =>
+        typeof(Freddles_AI1).GetMethod("Update", BindingFlags.Public | BindingFlags.Instance);
+    public HarmonyMethod Patch =>
+        new HarmonyMethod(
+            typeof(FreddlePatch).GetMethod("Prefix", BindingFlags.NonPublic | BindingFlags.Static)
+        );
+    public string debugName => "Freddles_AI1.Update";
+    public PatchType patchType => PatchType.Prefix;
+
+    private static void Prefix(Freddles_AI1 __instance)
+    {
+        typeof(Freddles_AI1)
+            .GetProperty("stade", BindingFlags.NonPublic | BindingFlags.Instance)
+            .SetValue(__instance, 0f);
+    }
+}
